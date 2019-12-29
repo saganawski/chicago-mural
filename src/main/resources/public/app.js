6ca2ed45761602b341c031a28f2ba8cd50df2c01
Ken Saganski <Ken.saganski@3won.com>
2019-12-29 16:26:26 -0600

    add ability to use pagination

--- src/main/resources/public/app.js
+++ src/main/resources/public/app.js
@@ -2,12 +2,18 @@ $(document).ready(function() {
 	
 	$('#nav').load("_nav.html");
 	
-    $.ajax({
-        url: "/murals"
-    }).then(function(data) {
-    	setCards(data._embedded.murals);
-    	setPaganation(data);
-    });
+	getMuralCards("/murals");
+    
+	function getMuralCards(url){
+		$('#mural-card').empty();
+		
+        $.ajax({
+            url: url
+        }).then(function(data) {
+        	setCards(data._embedded.murals);
+        	setPaganation(data);
+        });
+    }
     
     function setCards(murals){
     	for(let mural of murals){
@@ -34,6 +40,8 @@ $(document).ready(function() {
     function setPaganation(paganation){
     	pageNumber = paganation.page.number;
     	pageCount = paganation.page.totalPages;
+    	$('#pagination').empty();
+    	
     	for(var i=1; i <= pageCount; i++){
     		//set left chevron
     		if(i === 1){
@@ -46,11 +54,10 @@ $(document).ready(function() {
     			$('#pagination').append('<li id="right-chevron" class="waves-effect"><a href="#!"><i class="material-icons">chevron_right</i></a></li>');
     		}
     		
-    		//set ative class
     		setPaganationClassAttributes(pageNumber, pageCount);
     	}
     }
-    	// set active class info
+
     function setPaganationClassAttributes(pageNumber, pageCount){
     	// highlight active page
     	$('#pagination').find('li').find('a').eq(pageNumber + 1).parent("li").removeClass("waves-effect").addClass("active");
@@ -60,13 +67,49 @@ $(document).ready(function() {
     		$('#left-chevron').addClass("disabled");
     	}    	
     	//dissable right chenvron
-    	if(pageNumber === pageCount){
+    	if(pageNumber === (pageCount -1)){
     		$('#right-chevron').addClass("disabled");
     	}
     		
     }
 
-    //function to handle paganation
+    //function to handle paganation on click event
+    $('#pagination').click(function(event) {
+    	event.preventDefault();
+    	
+    	target = event.target.innerText;
+    	cheveronPageAction(target);
+    	
+    	pageNumber = event.target.text - 1;
+    	
+    	if(target != "chevron_left" && target != "chevron_right"){
+    		url = "murals/?page=" + pageNumber;
+    		getMuralCards(url);
+    	}
+    	    	
+    });
+    
+    function cheveronPageAction(target){
+    	currentPage = $('#pagination li.active').text();
+    	// do nothing 
+    	if((target != "chevron_left") && (target != "chevron_right")){
+    		return;
+    	}else if((target === "chevron_left" && currentPage === "1") || (target === "chevron_right" && currentPage === "15")){
+    		return;
+    	}
+    	
+    	if(target === "chevron_left"){
+    		goToPageNumber = currentPage -2
+    		url = "murals?page=" + goToPageNumber;
+        	getMuralCards(url);		
+    	}
+    	
+    	if(target === "chevron_right"){
+    		goToPageNumber = currentPage;
+    		url = "murals?page=" + goToPageNumber;
+        	getMuralCards(url);	
+    	}
+    }
     
     // colopse links to hamburger in mobile
     $('.sidenav').sidenav();
